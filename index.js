bf3d022f572e3cbc4fcde3d35355f3f8d951d9de
davidsese-002 <daviesese019@gmail.com>
2026-02-12 21:02:07 +0300

    Initial backend todo app with Express and MongoDB

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("To-Do API is running");
+});
+
+
+app.get("/todos", (req, res) => {
+  res.json([]);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
