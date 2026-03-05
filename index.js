1d177aec61230c530a4d6db826916f86f82fd4b3
davidsese-002 <daviesese019@gmail.com>
2026-03-05 19:50:05 +0300

    Fix routes structure and resolve merge conflicts

--- index.js
+++ index.js
@@ -2,27 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth.routes");
-const todoRoutes = require("./routes/todo.routes");
+const authRoutes = require("./frontend/src/routes/auth.routes");
+const todoRoutes = require("./frontend/src/routes/todo.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/todo_api")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
-
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+mongoose.connect("mongodb://127.0.0.1:27017/todo_db")
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
 });
 
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
